f8afa9b8d02b8df859d8f2c19650c7d3a45905ff
Njeri Mugeshi <nicolemugeshi@gmail.com>
2023-06-16 01:26:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+// Code your solution in this file// returnFirstTwoDrivers function
+const returnFirstTwoDrivers = function(drivers) {
+  return drivers.slice(0, 2);
+};
+
+// returnLastTwoDrivers function
+const returnLastTwoDrivers = function(drivers) {
+  return drivers.slice(-2);
+};
+
+// selectingDrivers array
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// createFareMultiplier function
+const createFareMultiplier = function(integer) {
+  return function(fare) {
+    return fare * integer;
+  };
+};
+
+// fareTripler function
+const fareTripler = createFareMultiplier(3);
+
+// selectDifferentDrivers function
+const selectDifferentDrivers = function(drivers, driverFunction) {
+  return driverFunction(drivers);
+};
